7e316c5c85797e0acdaf1331c0ab0cfcbb4d3cfc
Kha Mai <maianhkha.dev@gmail.com>
2018-01-04 22:46:16 +0700

    update highlights page

--- app/scripts/page-highlights.js
+++ app/scripts/page-highlights.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
   $('.highlight .header').on('click', function () {
     let self = this
-    $('.highlight .box').slideUp('fast')
+    $('.highlight .box').after().trigger('click')
     $(self).next().slideDown('fast')
+    $(self).closest('.highlight').addClass('active')
     $(self).slideUp('fast')
   })
   $('.highlight .box').after().on('click', function () {
     let self = this
     $(self).prev().slideDown('fast')
+    $(self).closest('.highlight').removeClass('active')
     $(self).slideUp('fast')
   })
 
